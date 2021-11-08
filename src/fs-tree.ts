e2270f9bdfddfe0a279ad1863c907dfd12fa4c39
Dan Čermák <dcermak@suse.com>
2021-11-08 09:39:52 +0100

    Split out Dir & FsSize out of main.ts

---
+++ src/fs-tree.ts
@@ -0,0 +1,80 @@
+/**
+ * Directory size as produced by the backend.
+ *
+ * This interface is the equivalent of the Dir struct from `dir.go`.
+ */
+export interface Dir {
+  path: string;
+  total_size: number;
+  files: { string: number } | {};
+  directories: { string: Dir } | {};
+}
+
+interface IFsSize {
+  readonly labels: string[];
+  readonly values: number[];
+  readonly ids: string[];
+  readonly parents: string[];
+}
+
+/**
+ * Class containing the data for visualizing the file system usage in a sunburst plot.
+ */
+export class FsSize implements IFsSize {
+  /**
+   * Array of the labels of each folder or file entry. This is the folder or
+   * file name.
+   */
+  readonly labels: string[] = [];
+
+  /** The size of each file or folder.
+   *
+   * For folders it's the size of the whole folder, for files it's the size of
+   * each file
+   */
+  readonly values: number[] = [];
+
+  /** A unique id of each file or folder. This is the full path of each entry. */
+  readonly ids: string[] = [];
+
+  /** The id (=full path) of the parent entry */
+  readonly parents: string[] = [];
+
+  /** Creates a new */
+  constructor(rootDir: Dir, maxDepth: number = -1) {
+    insertDirRec(rootDir, "", this, 0, maxDepth);
+  }
+}
+
+function insertDirRec(
+  dir: Dir,
+  prefix: string,
+  fsSize: IFsSize,
+  depth: number,
+  maxDepth: number = -1
+): void {
+  if (maxDepth > 0 && depth > maxDepth) {
+    return;
+  }
+  Object.entries(dir.files).forEach(([fname, size]) => {
+    fsSize.labels.push(fname);
+    fsSize.values.push(size);
+    fsSize.parents.push(prefix);
+    fsSize.ids.push(`${prefix}/${fname}`);
+  });
+
+  Object.entries(dir.directories).forEach(([dirName, subdir]) => {
+    fsSize.labels.push(dirName);
+    fsSize.values.push(subdir.total_size);
+    fsSize.parents.push(prefix);
+    fsSize.ids.push(`${prefix}/${dirName}`);
+
+    insertDirRec(
+      subdir,
+      `${prefix}/${subdir.path}`,
+      fsSize,
+      depth + 1,
+      maxDepth
+    );
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,9 @@
 import * as Plotly from "plotly.js";
+import { Dir, FsSize } from "./fs-tree";
 
-var layout = {
+const layout = {
   margin: { l: 0, r: 0, b: 0, t: 0 }
 };
-
-interface Dir {
-  path: string;
-  total_size: number;
-  files: { string: number };
-  directories: { string: Dir };
-}
-
-interface IFsSize {
-  readonly labels: string[];
-  readonly values: number[];
-  readonly ids: string[];
-  readonly parents: string[];
-}
-
-class FsSize implements IFsSize {
-  readonly labels: string[] = [];
-  readonly values: number[] = [];
-  readonly ids: string[] = [];
-  readonly parents: string[] = [];
-
-  constructor(rootDir: Dir, maxDepth: number = -1) {
-    insertDirRec(rootDir, "", this, 0, maxDepth);
-  }
-}
-
-function insertDirRec(
-  dir: Dir,
-  prefix: string,
-  fsSize: IFsSize,
-  depth: number,
-  maxDepth: number = -1
-): void {
-  if (maxDepth > 0 && depth > maxDepth) {
-    return;
-  }
-  Object.entries(dir.files).forEach(([fname, size]) => {
-    fsSize.labels.push(fname);
-    fsSize.values.push(size);
-    fsSize.parents.push(prefix);
-    fsSize.ids.push(`${prefix}/${fname}`);
-  });
-
-  Object.entries(dir.directories).forEach(([dirName, subdir]) => {
-    fsSize.labels.push(dirName);
-    fsSize.values.push(subdir.total_size);
-    fsSize.parents.push(prefix);
-    fsSize.ids.push(`${prefix}/${dirName}`);
-
-    insertDirRec(
-      subdir,
-      `${prefix}/${subdir.path}`,
-      fsSize,
-      depth + 1,
-      maxDepth
-    );
-  });
-}
 
 window.onload = (): void => {
   const [
