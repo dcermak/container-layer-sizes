7fe6304e369520ea956579aa3efd289480c45c98
Dan Čermák <dcermak@suse.com>
2022-03-31 14:15:43 +0200

    Wrap image info table in <details> element

--- src/ImageInformation.svelte
+++ src/ImageInformation.svelte
@@ -35,79 +35,82 @@
 <main>
   {#if $pageState === PageState.Plot || $pageState === PageState.Pulling}
     {#if currentTask !== undefined && currentTask.Image.ImageInfo !== undefined && currentTask.Image.ImageInfo !== null}
-      <table>
-        <thead>
-          <td>property</td>
-          <td>value</td>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Image</td>
-            <td>{currentTask.Image.Image}</td>
-          </tr>
-          <tr>
-            <td>Created</td>
-            <td>{currentTask.Image.ImageInfo.Created}</td>
-          </tr>
-          <tr>
-            <td>Docker Version</td>
-            <td>{currentTask.Image.ImageInfo.DockerVersion}</td>
-          </tr>
-          <tr>
-            <td>Architecture</td>
-            <td>{currentTask.Image.ImageInfo.Architecture}</td>
-          </tr>
-          <tr>
-            <td>Operating System</td>
-            <td>{currentTask.Image.ImageInfo.Os}</td>
-          </tr>
-          {#if currentTask.Image.ImageInfo.Tag !== ""}
+      <details open>
+        <summary>Image Properties:</summary>
+        <table>
+          <thead>
+            <td>property</td>
+            <td>value</td>
+          </thead>
+          <tbody>
             <tr>
-              <td>Tag</td>
-              <td>{currentTask.Image.ImageInfo.Tag}</td>
+              <td>Image</td>
+              <td>{currentTask.Image.Image}</td>
             </tr>
-          {/if}
-          {#if currentTask.Image.ImageInfo.Variant !== ""}
             <tr>
-              <td>Variant</td>
-              <td>{currentTask.Image.ImageInfo.Variant}</td>
+              <td>Created</td>
+              <td>{currentTask.Image.ImageInfo.Created}</td>
             </tr>
-          {/if}
-          {#if $pageState === PageState.Pulling && currentTask.pull_progress !== undefined && currentTask.pull_progress !== null}
             <tr>
-              <td>Layers</td>
-              <td>
-                {#each currentTask.Image.ImageInfo.Layers as layer}
-                  {layer}: {formatDownloadProgress(
-                    currentTask.pull_progress,
-                    layer
-                  )}<br />
-                {/each}
-              </td>
+              <td>Docker Version</td>
+              <td>{currentTask.Image.ImageInfo.DockerVersion}</td>
             </tr>
-          {/if}
-          {#if currentTask.Image.ImageInfo.Env !== undefined && currentTask.Image.ImageInfo.Env !== null && currentTask.Image.ImageInfo.Env.length > 0}
             <tr>
-              <td>Environment Variables</td>
-              <td>
-                {#each currentTask.Image.ImageInfo.Env as env}
-                  {env}<br />
-                {/each}
-              </td>
+              <td>Architecture</td>
+              <td>{currentTask.Image.ImageInfo.Architecture}</td>
             </tr>
-          {/if}
-          {#if currentTask.Image.ImageInfo.Labels !== undefined && currentTask.Image.ImageInfo.Labels !== null && Object.entries(currentTask.Image.ImageInfo.Labels).length > 0}
             <tr>
-              <td>Labels</td>
-              <td>
-                {#each Object.entries(currentTask.Image.ImageInfo.Labels) as [lbl, val]}
-                  {lbl}: {val}<br />
-                {/each}
-              </td>
+              <td>Operating System</td>
+              <td>{currentTask.Image.ImageInfo.Os}</td>
             </tr>
-          {/if}
-        </tbody>
-      </table>
+            {#if currentTask.Image.ImageInfo.Tag !== ""}
+              <tr>
+                <td>Tag</td>
+                <td>{currentTask.Image.ImageInfo.Tag}</td>
+              </tr>
+            {/if}
+            {#if currentTask.Image.ImageInfo.Variant !== ""}
+              <tr>
+                <td>Variant</td>
+                <td>{currentTask.Image.ImageInfo.Variant}</td>
+              </tr>
+            {/if}
+            {#if $pageState === PageState.Pulling && currentTask.pull_progress !== undefined && currentTask.pull_progress !== null}
+              <tr>
+                <td>Layers</td>
+                <td>
+                  {#each currentTask.Image.ImageInfo.Layers as layer}
+                    {layer}: {formatDownloadProgress(
+                      currentTask.pull_progress,
+                      layer
+                    )}<br />
+                  {/each}
+                </td>
+              </tr>
+            {/if}
+            {#if currentTask.Image.ImageInfo.Env !== undefined && currentTask.Image.ImageInfo.Env !== null && currentTask.Image.ImageInfo.Env.length > 0}
+              <tr>
+                <td>Environment Variables</td>
+                <td>
+                  {#each currentTask.Image.ImageInfo.Env as env}
+                    {env}<br />
+                  {/each}
+                </td>
+              </tr>
+            {/if}
+            {#if currentTask.Image.ImageInfo.Labels !== undefined && currentTask.Image.ImageInfo.Labels !== null && Object.entries(currentTask.Image.ImageInfo.Labels).length > 0}
+              <tr>
+                <td>Labels</td>
+                <td>
+                  {#each Object.entries(currentTask.Image.ImageInfo.Labels) as [lbl, val]}
+                    {lbl}: {val}<br />
+                  {/each}
+                </td>
+              </tr>
+            {/if}
+          </tbody>
+        </table>
+      </details>
     {/if}
   {/if}
 </main>
