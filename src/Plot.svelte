648581a852245615c2643bfa86a379ddcc1d1d91
Dan Čermák <dcermak@suse.com>
2022-03-31 14:14:24 +0200

    Improve sunburst graph display & remove pointless div

--- src/Plot.svelte
+++ src/Plot.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import Sunburst, { SunburstChartInstance } from "sunburst-chart";
+  import Sunburst from "sunburst-chart";
+  import type { SunburstChartInstance } from "sunburst-chart";
   import type { DataRouteReply } from "./types";
-  import { Dir, dirToDataNode } from "./fs-tree";
+  import { dirToDataNode } from "./fs-tree";
+  import type { Dir } from "./fs-tree";
   import { formatByte } from "./util";
 
   export let data: DataRouteReply | undefined = undefined;
@@ -39,63 +41,65 @@
 
     const root: Dir = data[digest as keyof DataRouteReply];
     graph = Sunburst();
-    graph.excludeRoot(true).minSliceAngle(0.4).data(dirToDataNode(root))(
-      plotDiv
-    );
+    graph
+      .excludeRoot(true)
+      .minSliceAngle(0.2)
+      .tooltipContent((_d, node) => `Size: <i>${formatByte(node.value)}</i>`)
+      .data(dirToDataNode(root))(plotDiv);
   };
 </script>
 
 {#if data !== undefined}
-  <div id="plot_config">
-    Select the layer to visualize:
-    <details>
-      <summary>Configure the table display</summary>
-      <label>
-        <input
-          type="checkbox"
-          bind:checked={showCreatedBy}
-          name="showCreatedBy"
-        />
-        Show Created By Column
-      </label>
-      <br />
-      <label for="trimDigestTo">Trim the digest to: </label>
-      <input type="number" bind:value={trimDigestTo} name="trimDigestTo" />
-    </details>
-    <form>
-      <table>
-        <thead>
-          <tr>
-            <td>layer</td>
-            <td>total size</td>
-            {#if showCreatedBy}
-              <td>created by</td>
-            {/if}
-          </tr>
-        </thead>
-        {#each layers as layer}
-          <tr>
-            <td>
-              <label>
-                <input
-                  type="radio"
-                  bind:group={digest}
-                  value={layer}
-                  name="layer"
-                />
-                {trimString(layer, trimDigestTo)}
-              </label>
-            </td>
-            <td>{printLayerSize(layer)}</td>
-            {#if showCreatedBy}
-              <td>{printCreatedBy(layer)}</td>
-            {/if}
-          </tr>
-        {/each}
-      </table>
-    </form>
-    <button on:click={drawPlot}>Plot this layer</button>
-  </div>
+  Select the layer to visualize:
+  <details open>
+    <summary>Configure the table display</summary>
+    <label>
+      <input
+        type="checkbox"
+        bind:checked={showCreatedBy}
+        name="showCreatedBy"
+      />
+      Show Created By Column
+    </label>
+    <br />
+    <label for="trimDigestTo">Trim the digest to: </label>
+    <input type="number" bind:value={trimDigestTo} name="trimDigestTo" />
+  </details>
+  <form on:submit|preventDefault={drawPlot}>
+    <table>
+      <thead>
+        <tr>
+          <td>layer</td>
+          <td>total size</td>
+          {#if showCreatedBy}
+            <td>created by</td>
+          {/if}
+        </tr>
+      </thead>
+      {#each layers as layer}
+        <tr>
+          <td>
+            <label>
+              <input
+                type="radio"
+                bind:group={digest}
+                value={layer}
+                name="layer"
+              />
+              {trimString(layer, trimDigestTo)}
+            </label>
+          </td>
+          <td>{printLayerSize(layer)}</td>
+          {#if showCreatedBy}
+            <td>{printCreatedBy(layer)}</td>
+          {/if}
+        </tr>
+      {/each}
+    </table>
+    <button type="submit" disabled={digest === undefined}
+      >Plot this layer</button
+    >
+  </form>
 
   <div id="plot_layers" />
 {/if}
