03a0cb2ba3c5c74c8436303967e9fc581636d21a
Dan Čermák <dcermak@suse.com>
2022-04-01 17:06:58 +0200

    Image layer comparison

--- src/History.svelte
+++ src/History.svelte
@@ -4,13 +4,31 @@
     type ImageEntry,
     type ImageHistory
   } from "./backend-storage";
+  import Sunburst from "sunburst-chart";
   import ContainerImageTable from "./ContainerImageTable.svelte";
+  import LayerSelection from "./LayerSelection.svelte";
+  import LayerSelectionConfig from "./LayerSelectionConfig.svelte";
+  import type { DataRouteReply } from "./types";
+  import { formatByte } from "./util";
+  import {
+    compareDirsToDataNodes,
+    findNodeInOtherGraph,
+    Layer
+  } from "./fs-tree";
+
+  let showCreatedBy: boolean = true;
+  let trimDigestTo: number = -1;
+
+  let leftPlot: HTMLElement;
+  let rightPlot: HTMLElement;
 
   let allImagesPromise: Promise<ImageEntry[]> | undefined = undefined;
   let imageHistoryFetchPromises: Map<
     number,
     Promise<ImageHistory> | undefined
   > = new Map();
+  let selectedLayers: Record<number, Record<string, string | undefined>> = {};
+  let savedHistories: Record<number, ImageHistory> = {};
 
   const backend = new BackendStorage();
 
@@ -21,13 +39,90 @@
   };
 
   const fetchHistoryOfImage = (id: number) => {
-    imageHistoryFetchPromises.set(id, backend.fetchImageHistory(id));
+    selectedLayers[id] = {};
+    imageHistoryFetchPromises.set(
+      id,
+      backend.fetchImageHistory(id).then((hist) => {
+        savedHistories[id] = hist;
+        return hist;
+      })
+    );
     imageHistoryFetchPromises = imageHistoryFetchPromises;
+  };
+
+  const compareLayers = () => {
+    const res: string[] = [];
+    const layersToCompare: Layer[] = [];
+
+    Object.entries(selectedLayers).forEach(([id, layers]) => {
+      Object.entries(layers).forEach(([digest, layer]) => {
+        layersToCompare.push(
+          savedHistories[id as unknown as number].History[digest].Contents[
+            layer as keyof DataRouteReply
+          ]
+        );
+        res.push(layer);
+      });
+    });
+    if (res.length != 2) {
+      alert(`You can only compare two layers, but you selected ${res.length}`);
+      return;
+    }
+
+    leftPlot.innerHTML = "";
+    rightPlot.innerHTML = "";
+
+    const [dl, dr] = compareDirsToDataNodes(
+      layersToCompare[0],
+      layersToCompare[1]
+    );
+
+    const [gl, gr] = [
+      { data: dl, div: leftPlot },
+      { data: dr, div: rightPlot }
+    ].map(({ data, div }) => {
+      const g = Sunburst();
+
+      g
+        .excludeRoot(true)
+        .minSliceAngle(0.2)
+        .color((d) => d.color)
+        .tooltipContent((_d, node) => `Size: <i>${formatByte(node.value)}</i>`)
+        .data(data)(div);
+
+      return g;
+    });
+
+    gl.onClick((n) => {
+      gl.focusOnNode(n);
+      const otherFocus = findNodeInOtherGraph(n, gr.data());
+      if (otherFocus !== undefined && otherFocus !== null) {
+        gr.focusOnNode(otherFocus);
+      }
+    });
+    gr.onClick((n) => {
+      gr.focusOnNode(n);
+      const otherFocus = findNodeInOtherGraph(n, gl.data());
+      if (otherFocus !== undefined && otherFocus !== null) {
+        gl.focusOnNode(otherFocus);
+      }
+    });
+    gr.data;
   };
 </script>
 
 <button on:click={fetchAllImages}>Fetch all images on the backend</button><br />
 
+<!-- <table> -->
+<!--   <tr> -->
+<!--     <td> -->
+<div bind:this={leftPlot} />
+<!-- </td> -->
+<!-- <td> -->
+<div bind:this={rightPlot} />
+<!-- </td> -->
+<!--   </tr> -->
+<!-- </table> -->
 {#if allImagesPromise !== undefined}
   {#await allImagesPromise}
     Fetching all images from the backend
@@ -35,29 +130,43 @@
     {#if allImages.length == 0}
       No images are stored in the backend
     {:else}
-      {#each allImages as img}
-        {img.Name}
-        {#if imageHistoryFetchPromises.get(img.ID) === undefined}
-          <button on:click={() => fetchHistoryOfImage(img.ID)}
-            >Fetch the history of this image</button
-          >
-        {:else}
-          {#await imageHistoryFetchPromises.get(img.ID)}
-            Fetching the history of {img.Name}
-          {:then data}
-            {#each Object.entries(data.History) as entry}
-              {entry[0]}<br />
-              <ContainerImageTable
-                containerImageName={img.Name}
-                imageInfo={entry[1].InspectInfo}
-                showPullProgress={false}
-                open={false}
-              />
-            {/each}
-          {/await}
-        {/if}
-        <br />
-      {/each}
+      <br />
+      <LayerSelectionConfig bind:showCreatedBy bind:trimDigestTo />
+
+      <form on:submit|preventDefault={compareLayers}>
+        {#each allImages as img}
+          <h1>{img.Name}</h1>
+          {#if imageHistoryFetchPromises.get(img.ID) === undefined}
+            <button on:click={() => fetchHistoryOfImage(img.ID)}
+              >Fetch the history of this image</button
+            >
+          {:else}
+            {#await imageHistoryFetchPromises.get(img.ID)}
+              Fetching the history of {img.Name}
+            {:then data}
+              {#each Object.entries(data.History) as entry}
+                <h2>{entry[0]}</h2>
+                <br />
+                <ContainerImageTable
+                  containerImageName={img.Name}
+                  imageInfo={entry[1].InspectInfo}
+                  showPullProgress={false}
+                  open={false}
+                />
+                <br />
+                <LayerSelection
+                  data={entry[1].Contents}
+                  {showCreatedBy}
+                  {trimDigestTo}
+                  bind:digest={selectedLayers[img.ID][entry[0]]}
+                />
+              {/each}
+            {/await}
+          {/if}
+          <br />
+        {/each}
+        <button type="submit">Compare the selected layers</button>
+      </form>
     {/if}
   {/await}
 {/if}
--- src/fs-tree.ts
+++ src/fs-tree.ts
@@ -1,4 +1,4 @@
-import type { Node } from "sunburst-chart";
+import type { DataNode, Node } from "sunburst-chart";
 
 /**
  * Directory size as produced by the backend.
@@ -8,12 +8,101 @@ import type { Node } from "sunburst-chart";
 export interface Dir {
   readonly dirname: string;
   readonly total_size: number;
-  readonly files: { string: number } | {};
-  readonly directories: { string: Dir } | {};
+  readonly files: Record<string, number>;
+  readonly directories: Record<string, Dir>;
 }
 
 export interface Layer extends Dir {
   readonly CreatedBy: string;
+}
+
+const colorOfSizeDif = (
+  sizeL: number,
+  sizeR?: number
+): "red" | "green" | "blue" | "yellow" => {
+  if (sizeR === undefined) {
+    return "blue";
+  } else if (sizeL === sizeR) {
+    return "yellow";
+  } else if (sizeL > sizeR) {
+    return "red";
+  } else {
+    return "green";
+  }
+};
+
+const getColorOfDentry = (
+  dirL: Dir,
+  dirR?: Dir
+): "red" | "green" | "blue" | "yellow" => {
+  return colorOfSizeDif(dirL.total_size, dirR?.total_size);
+};
+
+const getColorOfFileEntry = (
+  fname: string,
+  dirL: Dir,
+  dirR?: Dir
+): "red" | "green" | "blue" | "yellow" => {
+  return colorOfSizeDif(dirL.files[fname], dirR?.files[fname]);
+};
+
+export function compareDirsToDataNodes(dirL?: Dir, dirR?: Dir): [Node?, Node?] {
+  let dL =
+    dirL === undefined
+      ? undefined
+      : {
+          name: dirL.dirname,
+          value: 0,
+          children: [] as Node[],
+          color: getColorOfDentry(dirL, dirR)
+        };
+  let dR =
+    dirR === undefined
+      ? undefined
+      : {
+          name: dirR.dirname,
+          value: 0,
+          children: [] as Node[],
+          color: getColorOfDentry(dirR, dirL)
+        };
+
+  Object.entries(dirL?.files ?? []).forEach(([fname, size]) => {
+    dL.children.push({
+      name: fname,
+      value: size,
+      color: getColorOfFileEntry(fname, dirL, dirR)
+    });
+  });
+
+  Object.entries(dirR?.files ?? []).forEach(([fname, size]) => {
+    dR.children.push({
+      name: fname,
+      value: size,
+      color: getColorOfFileEntry(fname, dirR, dirL)
+    });
+  });
+
+  Object.entries(dirL?.directories ?? []).forEach(([dirName, subdir]) => {
+    const [left, _right] = compareDirsToDataNodes(
+      subdir,
+      dirR?.directories[dirName]
+    );
+    if (left !== undefined) {
+      dL.children.push(left);
+    }
+  });
+
+  Object.entries(dirR?.directories ?? []).forEach(([dirName, subdir]) => {
+    const [right, _left] = compareDirsToDataNodes(
+      subdir,
+      dirL?.directories[dirName]
+    );
+    if (right !== undefined) {
+      dR.children.push(right);
+    }
+  });
+
+  return [dL, dR];
 }
 
 export function dirToDataNode(dir: Dir): Node {
@@ -32,4 +121,31 @@ export function dirToDataNode(dir: Dir): Node {
   });
 
   return d;
+}
+
+export function findNodeInOtherGraph(
+  nodeLeft: Node | null | undefined,
+  rootRight: Node | null | undefined
+): Node | undefined {
+  if (nodeLeft === null || nodeLeft === undefined) {
+    return undefined;
+  }
+
+  const path: string[] = [];
+  let n: DataNode | undefined | null = nodeLeft?.__dataNode;
+
+  while (n !== null && n !== undefined) {
+    path.push(n.data.name);
+    n = n.parent;
+  }
+
+  let res: Node | null = rootRight.__dataNode;
+  for (let i = path.length - 1; i > 0; i--) {
+    if (res === null) {
+      return undefined;
+    }
+    res = res.children.find((c) => c.data.name === path[i - 1]);
+  }
+
+  return res?.data;
 }
